ef0ac2cd43bdf963417da578528275d7d62f111c
Joshua <joshua43210@gmail.com>
2019-05-07 11:34:45 +0000

    add images

--- socket/server.js
+++ socket/server.js
@@ -6,6 +6,7 @@ var io = require("socket.io")(server);
 server.listen(8080, () => {console.log("website connected at port 8080")});
 
 app.use(express.static('web'));
+express.static.mime.define({'text/css': ['css']});
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
@@ -13,14 +14,22 @@ app.get('/web-script.js', function(req, res) {
     res.sendFile(__dirname + "/web-script.js");
 });
 app.get('/jquery.js', function(req, res) {
-    console.log("wut");
     res.sendFile(__dirname + "/lib/jquery.min.js");
+});
+app.get('/normalize.css', function(req, res) {
+    res.sendFile(__dirname + "/normalize.css");
+});
+app.get('/skeleton.css', function(req, res) {
+    res.sendFile(__dirname + "/skeleton.css");
+});
+app.get('/custom.css', function(req, res) {
+    res.sendFile(__dirname + "/custom.css");
 });
 
 io.on("connection", function(socket) {
     console.log("a client connected");
     socket.on("pi:server", (data) => {
-        console.log("server received from pi: " + data.msg);
+        console.log("server received from pi: " + data.code);
         io.emit("server:website", data);
     });
 });
--- socket/web-script.js
+++ socket/web-script.js
@@ -1,6 +1,12 @@
 var socket = io();
 socket.on('connect', function(){console.log("connected")});
 socket.on('server:website', function(data) {
-    console.log(data);
+    // console.log(data);
+    var code = data.code;
+    var name = data.name;
+    $("#gesture").text(name.substring(0, name.length - 4));
+
+    $("img").hide();
+    $("#image" + code).show();
 });
 socket.on('disconnect', function(){});
